11726373270b1da585e91beb94b8d9ff1457c20d
v-immor <117469446+v-immor@users.noreply.github.com>
2022-11-25 22:18:54 +0800

    Alpha (#2)
    * feat: export react-transition-group
    
    * publish: 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,10 @@ export { DEFAULT_ANIMATIONS } from './animation'
 
 export { createAnimationStyles, getEnterOrExitAnimationStyle, DEFAULT_WRAPPER_STYLE } from './utils'
 export type { AnimationProps } from './utils'
+
+export { Transition, TransitionGroup, CSSTransition, SwitchTransition } from 'react-transition-group'
+export type { TransitionStatus } from 'react-transition-group'
+export type { CSSTransitionClassNames, CSSTransitionProps } from 'react-transition-group/CSSTransition'
+export type { SwitchTransitionProps, modes } from 'react-transition-group/SwitchTransition'
+export type { EndHandler, EnterHandler, ExitHandler, TransitionActions, TimeoutProps, EndListenerProps, TransitionProps } from 'react-transition-group/Transition'
+export type { IntrinsicTransitionGroupProps, ComponentTransitionGroupProps, TransitionGroupProps } from 'react-transition-group/TransitionGroup'
